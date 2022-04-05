73f6af2c17d4d01736b0f46d5749960478e06cfe
malenabensadon <bensadonm@udesa.edu.ar>
2022-04-05 10:14:36 -0300

    prueba

---
+++ proyectoProg2/controllers/productController.js
@@ -0,0 +1,13 @@
+const productsData = require('../db/dataProducts');
+
+const productController = {
+
+    index:
+    
+    function (req,res){
+    return res.render(dataProducts.lista)
+
+}
+}
+
+module.exports = productsData;
