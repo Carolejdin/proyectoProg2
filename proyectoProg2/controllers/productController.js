445cca78d0ff8845fad56da2731cf498ece43a63
carolejdin <cejdin@udesa.edu.ar>
2022-04-09 15:33:31 -0300

    agregue product.ejs y lo hice edinamico, pagregue el productadd.ejs y cambie algo en productController.js

--- proyectoProg2/controllers/productController.js
+++ proyectoProg2/controllers/productController.js
@@ -1,12 +1,13 @@
+const libros = require('../db/data');
 var users = require('../db/data');
 
 //estamos haciendo alusion en cada una de las lineas a las vistas de cada uno: index.ejs, register.ejs y login.ejs
 const productController = {
     product: function(req, res) {
-        res.render('product', { listaUsuarios: libros.productos});
+        res.render('product', { listaProduct: libros.productos, listaUsuarios: libros.usuarios});
     },
     productAdd: function(req, res) {
-        res.render('product-add', { listaUsuarios: libros.productos});
+        res.render('product-add', { listaUsuarios2: libros.productos});
     },
    
 }
