b39f8a0b62d1bd25f3a75a0a834d85f6148cd3e7
System Administrator <root@MacBook-de-Mora.local>
2022-04-10 18:12:49 -0300

    header y header logueado

--- proyectoProg2/controllers/indexController.js
+++ proyectoProg2/controllers/indexController.js
@@ -4,7 +4,7 @@ var main = require('../db/data');
 //estamos haciendo alusion en cada una de las lineas a las vistas de cada uno: index.ejs, register.ejs y login.ejs
 const indexController = {
     index: function(req, res) {
-        res.render('index', { listaProductos: libros.productos});
+        res.render('index', { listaProductos: libros.productos, query: req.query});
     },
     login: function(req, res) {
         res.render('login',  );
