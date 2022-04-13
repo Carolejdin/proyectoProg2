d0cbd4663d19740906811bf1d77160e90657647d
carolejdin <cejdin@udesa.edu.ar>
2022-04-13 16:54:56 -0300

    comentario

--- proyectoProg2/routes/product.js
+++ proyectoProg2/routes/product.js
@@ -5,7 +5,7 @@ var controller = require('../controllers/productController')
 router.get('/productAdd', controller.productAdd);
 router.get ('/searchResults', controller.searchResults);
 router.get('/:id', controller.product);
-
+//a
 
 
 module.exports = router;
