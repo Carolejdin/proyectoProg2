b6ae3d6c6196f4549ac899d5be4c19bddd971026
carolejdin <cejdin@udesa.edu.ar>
2022-04-17 15:41:54 -0300

    probando borrar maqueta

--- proyectoProg2/controllers/productController.js
+++ proyectoProg2/controllers/productController.js
@@ -1,7 +1,7 @@
 const libros = require('../db/data');
 
 //estamos haciendo alusion en cada una de las lineas a las vistas de cada uno: index.ejs, register.ejs y login.ejs
-//prueba
+//pruebaaaaa
 const productController = {
     product: function(req, res) {
         res.render('product', { id: req.params.id, listaProduct: libros.productos, comentarios: libros.comentarios});
