a9ea2c90594ccb29a0effc4086b41cd90ce2857e
Mora Ludin <moraludin@MacBook-de-Mora.local>
2022-06-06 11:09:13 -0300

    instalaciones

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,10 @@
 //const libros = require('../db/data');
-const db= require ('../database/models');
+/* const db= require ('../database/models');
 const op = db.Sequelize.op;
-let resultados = req.query.search
+let resultados = req.query.search */
 //estamos haciendo alusion en cada una de las lineas a las vistas de cada uno: index.ejs, register.ejs y login.ejs
 //pruebaaaaa
-const productController = {
+// const productController = {
   //  product: function (req, res) {
    //     res.render('product', { id: req.params.id, listaProduct: libros.productos, comentarios: libros.comentarios });
   //  },
@@ -14,19 +14,21 @@ const productController = {
   //  searchResults: function (req, res) {
   //      res.render('searchResults', { listaProductos: libros.productos });
    // },
-searchResults:function(req,res){
+/* searchResults:function(req,res){ */
     
-   db.products.findAll( //products es el alias del modelo
-       {
+   /* db.products.findAll({//products es el alias del modelo
          where:{ model:{[op.gt]:"%" + req.query.search }
-        }
+        })
       
    
          .then (function(products){
            return res.render ('searchResults', {resultados: resultados})
          })
-         .catch(error => console.log(error))
+         .catch(error => console.log(error)) */
  
-})
+/* }
+}
+ */
 
-module.exports = productController;
+
+/* module.exports = productController; */
