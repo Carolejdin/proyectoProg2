96e94833d402dd6969e0923bd2762dde2a5b0b96
Malena Bensadon <malenabensadon@MacBook-Pro-de-Malena.local>
2022-06-06 12:12:57 -0300

    register con errores

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,4 @@
-//const libros = require('../db/data');
+/* //const libros = require('../db/data');
 //var data = require('../db/data');
 const libros = require('../database/models');
 const data = require('../database/models');
@@ -14,4 +14,71 @@ const usersController = {
 
 }
 
-module.exports = usersController;
+module.exports = usersController; */
+
+const usersController = {
+    profile: function (req, res) {
+        res.render('profile', { listaUsuarios: data.usuarios, listaProductos: libros.productos });
+    },
+    profileEdit: function (req, res) {
+        res.render('profileEdit', { listaUsuarios: data.usuarios });
+    }, 
+
+}
+
+
+const db = require('../database/models');
+const bcrypt = require('bcryptjs');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+
+let usuario = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    birthDay: req.body.birthDay,
+    DNI: req.body.DNI,
+    profilePic: req.body.profilePic,
+};
+
+db.User.create(usuario)
+.then((results)=> {
+    return results.redirect('/')
+}).catch(error => console.log(error))
+
+const userController = {
+    register: function(req, res){
+        //return res.render('register');
+
+       if(req.session.user != undefined){
+           return res.redirect('/')
+       } else{
+           return res.render('register');
+       }
+    },
+     
+}
+
+let errors = {};
+
+if(req.body.username == ""){
+    errors.message = "Complete el nombre de usuario";
+    res.locals.errors = errors;
+    return res.render('register');
+}
+
+if(req.body.email == ""){
+    errors.message = "Complete el email";
+    res.locals.errors = errors;
+    return res.render('register');
+}
+
+if(req.body.password == ""){
+    errors.message = "Complete el email";
+    res.locals.errors = errors;
+    return res.render('register');
+}
+    
+    
+
+module.exports = usersController; 
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/usersController')
 
+let multer = require('multer');
+let path = require('path');
+
+//confirgurar multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../public/images')) //Usamos path.join para evitar problemas de rutas. __dirname da la posición exacta de la carpeta en la que está el archivo. Luego desde ahí nos movemos hasta la carpeta public.
+      //Las carpetas deben existir.
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+  var upload = multer({ storage: storage })
+
 router.get('/profile', controller.profile);
 router.get('/profileEdit', controller.profileEdit);
+router.get('/register', userController.register);
 
 
 module.exports = router;
