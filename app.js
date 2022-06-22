bb1d135c0eaebff8872c6821f539c64dc7828f89
Mora Ludin <moraludin@MacBook-de-Mora.local>
2022-06-21 22:36:18 -0300

    correcion mini

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // formulario --> pasar el objeto literal a formato json 
+app.use(express.urlencoded({ extended: false })); // formulario --> todo lo que se mande por el formulario es en forma de objeto literal
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(function(req, res, next){
